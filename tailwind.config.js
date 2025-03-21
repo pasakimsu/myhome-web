5a6f8fd4cd751c66069f820533f4b862d176ae86
pasakimsu <nalb@naver.com>
2025-03-21 22:12:33 +0900

    💡 변경 내용

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "shadow-[0px_6px_12px_rgba(0,0,0,0.4),_0px_-3px_6px_rgba(255,255,255,0.2)]",
+    "hover:shadow-[0px_4px_10px_rgba(0,0,0,0.5),_0px_-2px_5px_rgba(255,255,255,0.2)]",
+    "active:shadow-[0px_2px_6px_rgba(0,0,0,0.6),_0px_-1px_3px_rgba(255,255,255,0.1)]",
+    "shadow-[0px_6px_12px_rgba(0,0,0,0.5),_inset_0px_-3px_6px_rgba(255,255,255,0.3)]",
+    "active:shadow-[inset_0px_4px_8px_rgba(0,0,0,0.5),_inset_0px_-3px_6px_rgba(255,255,255,0.3)]",
+    "translate-y-[2px]",
+    "translate-y-[3px]",
+    "translate-y-[4px]",
+    "hover:translate-y-[2px]",
+    "active:translate-y-[3px]",
+    "active:translate-y-[4px]"
+  ],
   theme: {
     extend: {
       colors: {
